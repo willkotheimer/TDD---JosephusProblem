95fd67d3922819091a7337371e56c02866907baa
wkotheimer <wkotheimer@gmail.com>
2021-04-08 21:08:40 -0500

    Adding Jospehus Problem

---
+++ CheckSurvivorJosephus.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace JosephusProblem.Tests
+{
+    public class CheckSurvivorJosephus
+    {
+        [Theory]
+        [InlineData(10,3,4)]
+        [InlineData(7, 2, 7)]
+        public void the_person_left_behind_matches_expected(int numberOfPeople, int killingInterval, int survingMember)
+        {
+            var situation = new LastPersonStandingClass();
+            var lps = situation.LastPersonStanding(numberOfPeople, killingInterval);
+            Assert.Equal(survingMember, lps);
+        }
+    }
+}
